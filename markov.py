83d1336219395c685302af85e49b8ba9434ca74e
HeoJinuk <boan3693@gmail.com>
2024-11-15 16:55:07 +0900

    initialize
    initialize library

---
+++ markov.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from graphviz import Digraph
+
+
+class Markov:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def save_to_csv(self):
+        pass
+
+    @property
+    def array(self):
+        pass
+
+    @property
+    def node_names(self):
+        pass
